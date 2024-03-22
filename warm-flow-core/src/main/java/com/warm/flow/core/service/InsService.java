0175dc907437cfec07a56ef5b5a49d6aeff12537
holly <lihu5297@qq.com>
2024-03-22 15:47:10 +0800

    流程实例使用注释

--- warm-flow-core/src/main/java/com/warm/flow/core/service/InsService.java
+++ warm-flow-core/src/main/java/com/warm/flow/core/service/InsService.java
@@ -17,7 +17,7 @@ public interface InsService extends IWarmService<Instance> {
     /**
      * 根据id集合进行查询 行锁
      *
-     * @param ids
+     * @param ids: 流程实例集合[必传]
      * @return
      */
     List<Instance> getByIdWithLock(List<Long> ids);
@@ -25,8 +25,14 @@ public interface InsService extends IWarmService<Instance> {
     /**
      * 根据开始的节点,业务id集合开启流程
      *
-     * @param businessId
-     * @param flowParams
+     * @param businessId: 业务id[必传]
+     * @param flowParams: 包含流程相关参数的对象
+     *        - flowCode:流程编码 [必传]
+     *        - createBy:创建人帐号[建议传]
+     *        - nickname:创建人昵称[按需传输]
+     *        - tenantId:租户id [按需传输]
+     *        - variable:流程变量[按需传输]
+     *        - ext:扩展字段[按需传输]
      * @return
      */
     Instance start(String businessId, FlowParams flowParams);
@@ -35,8 +41,17 @@ public interface InsService extends IWarmService<Instance> {
     /**
      * 根据实例id，流程跳转，一般是开始节点后第一个节点，用来提交申请，此时不可有同时两个代办任务
      *
-     * @param instanceId
-     * @param flowParams
+     * @param instanceId:流程实例id[必传]
+     * @param flowParams:包含流程相关参数的对象
+     *        - skipType:跳转类型(PASS审批通过 REJECT驳回) [必传]
+     *        - nodeCode:节点编码 [如果指定跳转节点,必传]
+     *        - permissionFlag:办理人权限标识[必传]
+     *        - skipCondition:跳转条件[互斥网关必传]
+     *        - message:审批意见  [按需传输]
+     *        - createBy:办理人帐号[建议传]
+     *        - nickname:办理人昵称[按需传输]
+     *        - variable:流程变量[按需传输]
+     *        - variableTask:任务变量[按需传输]
      * @return
      */
     Instance skipByInsId(Long instanceId, FlowParams flowParams);
@@ -44,16 +59,24 @@ public interface InsService extends IWarmService<Instance> {
     /**
      * 根据任务id，流程跳转
      *
-     * @param taskId
-     * @param flowParams
-     * @return
+     * @param taskId:流程任务id[必传]
+     * @param flowParams:包含流程相关参数的对象
+     *        - skipType:跳转类型(PASS审批通过 REJECT驳回) [必传]
+     *        - nodeCode:节点编码 [如果指定跳转节点,必传]
+     *        - permissionFlag:办理人权限标识[必传]
+     *        - skipCondition:跳转条件(变量值：如flag=xxx)[互斥网关必传]
+     *        - message:审批意见  [按需传输]
+     *        - createBy:办理人帐号[建议传]
+     *        - nickname:办理人昵称[按需传输]
+     *        - variable:流程变量[按需传输]
+     *        - variableTask:任务变量[按需传输]     * @return
      */
     Instance skip(Long taskId, FlowParams flowParams);
 
     /**
      * 根据实例id，删除流程
      *
-     * @param instanceIds
+     * @param instanceIds: 流程实例集合[必传]
      * @return
      */
     boolean remove(List<Long> instanceIds);
