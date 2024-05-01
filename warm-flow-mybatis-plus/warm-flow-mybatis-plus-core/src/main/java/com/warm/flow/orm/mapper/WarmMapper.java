b172ece301f348a1dca3dc5497803eec8f7f05f0
笨小孩 <290631660@qq.com>
2024-05-01 17:12:19 +0800

    修改mybatis-plus版本号

--- warm-flow-mybatis-plus/warm-flow-mybatis-plus-core/src/main/java/com/warm/flow/orm/mapper/WarmMapper.java
+++ warm-flow-mybatis-plus/warm-flow-mybatis-plus-core/src/main/java/com/warm/flow/orm/mapper/WarmMapper.java
@@ -1,14 +1,6 @@
 package com.warm.flow.orm.mapper;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.warm.tools.utils.page.Page;
-import org.apache.ibatis.annotations.Param;
-
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * BaseMapper接口
