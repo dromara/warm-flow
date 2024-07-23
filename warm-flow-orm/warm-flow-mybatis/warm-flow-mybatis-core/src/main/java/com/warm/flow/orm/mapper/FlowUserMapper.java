8468fddeed8016af0e069c4d703a2c242b2861a4
warm <290631660@qq.com>
2024-07-23 13:55:16 +0800

    [fix] 修复加签批量提交报错

--- warm-flow-orm/warm-flow-mybatis/warm-flow-mybatis-core/src/main/java/com/warm/flow/orm/mapper/FlowUserMapper.java
+++ warm-flow-orm/warm-flow-mybatis/warm-flow-mybatis-core/src/main/java/com/warm/flow/orm/mapper/FlowUserMapper.java
@@ -55,11 +55,11 @@ public interface FlowUserMapper extends WarmMapper<FlowUser> {
                              @Param("logicNotDeleteValue") String logicNotDeleteValue);
 
     List<FlowUser> listByAssociatedAndTypes(@Param("types") String[] types
-            , @Param("associateds") List<Long> processedBys
+            , @Param("associateds") List<Long> associateds
             , @Param("entity") FlowUser entity);
 
     List<FlowUser> listByProcessedBys(@Param("types") String[] types
-            , @Param("associateds") List<String> processedBys
+            , @Param("processedBys") List<String> processedBys
             , @Param("entity") FlowUser entity);
 
 }
