03abbb652adc7ae080269032de2d7b768729b347
xiaoxiaoliu889 <2276382034@qq.com>
2024-08-15 19:47:06 +0800

    [fix] 修复SQL语句

--- sql/mysql/v1-upgrade/warm-flow_1.2.4.sql
+++ sql/mysql/v1-upgrade/warm-flow_1.2.4.sql
@@ -61,4 +61,4 @@ ALTER TABLE `flow_instance`
     ADD COLUMN `listener_type` varchar(100) DEFAULT NULL COMMENT '监听器类型' AFTER `activity_status`;
 
 ALTER TABLE `flow_instance`
-    ADD COLUMN `listener_path` varchar(100) DEFAULT NULL COMMENT '监听器路径' AFTER `listener_type`;
+    ADD COLUMN `listener_path` varchar(400) DEFAULT NULL COMMENT '监听器路径' AFTER `listener_type`;
