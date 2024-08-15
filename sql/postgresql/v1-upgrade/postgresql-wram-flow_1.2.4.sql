bb56e43fcf7245b4570118fee4e8d5dca7dd5c79
xiaoxiaoliu889 <2276382034@qq.com>
2024-08-15 20:56:56 +0800

    [fix] 修复SQL语句

--- sql/postgresql/v1-upgrade/postgresql-wram-flow_1.2.4.sql
+++ sql/postgresql/v1-upgrade/postgresql-wram-flow_1.2.4.sql
@@ -51,5 +51,5 @@ COMMENT ON COLUMN flow_instance.activity_status IS '流程激活状态（0挂起
 ALTER TABLE flow_definition ADD listener_type varchar(100) NULL;
 COMMENT ON COLUMN flow_definition.listener_type IS '监听器类型';
 
-ALTER TABLE flow_definition ADD listener_path varchar(100) NULL;
+ALTER TABLE flow_definition ADD listener_path varchar(400) NULL;
 COMMENT ON COLUMN flow_definition.listener_path IS '监听器路径';
