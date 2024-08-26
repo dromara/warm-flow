b5b8331d9fa3eebeb8bcc868171b20a636ebe7d7
warm <290631660@qq.com>
2024-08-26 14:01:59 +0800

    [update] 修改sql脚本，关于流程状态字段

--- sql/mysql/v1-upgrade/warm-flow_1.2.5.sql
+++ sql/mysql/v1-upgrade/warm-flow_1.2.5.sql
@@ -3,3 +3,14 @@ ALTER TABLE `flow_instance`
 
 ALTER TABLE `flow_instance`
     ADD COLUMN `listener_path` varchar(400) DEFAULT NULL COMMENT '监听器路径' AFTER `listener_type`;
+
+-- 流程实例和流程历史表修改流程状态字段类型为字符串类型-varchar
+ALTER TABLE `flow_instance` MODIFY
+    COLUMN `flow_status` varchar(20)
+    DEFAULT NULL COMMENT '流程状态（0待提交 1审批中 2 审批通过 3自动通过 8已完成 9已退回 10失效）'
+    AFTER `variable`;
+
+ALTER TABLE `flow_his_task` MODIFY
+    COLUMN `flow_status` varchar(20)
+    DEFAULT NULL COMMENT '流程状态（0待提交 1审批中 2 审批通过 3自动通过 8已完成 9已退回 10失效）'
+    AFTER `skip_type`;
