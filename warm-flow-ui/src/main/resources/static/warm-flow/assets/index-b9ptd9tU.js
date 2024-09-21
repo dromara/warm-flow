4c4c96b2e60343c75059d7d09a76e24ec04f3966
warm <290631660@qq.com>
2024-09-21 16:32:28 +0800

    [update] 描述

--- warm-flow-ui/src/main/resources/static/warm-flow/assets/index-b9ptd9tU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,d as c,o as n,c as s,p as t,e as a}from"./index-CHQH9-TC.js";const r={};function _(d,i){const e=c("svg-icon");return n(),s("div",null,[t(" 表单构建 "),a(e,{"icon-class":"build"})])}const p=o(r,[["render",_]]);export{p as default};
