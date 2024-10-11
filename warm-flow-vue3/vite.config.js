490343417c1d4887af437d4ffc9847e99d008cb7
warm <290631660@qq.com>
2024-10-11 13:11:23 +0800

    [update] 设计器独立持续开发

--- warm-flow-vue3/vite.config.js
+++ warm-flow-vue3/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode, command }) => {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-    base: "/warm-flow-ui",//VITE_APP_ENV === 'production' ? '/warmjars' : '/',
+    base: "/warm-flow-ui",
     plugins: createVitePlugins(env, command === 'build'),
    // 设置打包后的路径
     build: {
