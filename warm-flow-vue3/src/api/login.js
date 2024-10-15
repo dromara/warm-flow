0e7a08e63c1db37cc25a58672287a5e59f50af21
warm <290631660@qq.com>
2024-10-15 13:13:59 +0800

    [remove] 删除多余的代码和文件

--- warm-flow-vue3/src/api/login.js
+++ warm-flow-vue3/src/api/login.js
@@ -1,32 +1,5 @@
 import request from '@/utils/request'
 
-// 登录方法
-export function login(username, password, code, uuid) {
-  const data = {
-    username,
-    password,
-    code,
-    uuid
-  }
-  return request({
-    url: '/login',
-    headers: {
-      isToken: false,
-      repeatSubmit: false
-    },
-    method: 'post',
-    data: data
-  })
-}
-
-// 获取用户详细信息
-export function getInfo() {
-  return request({
-    url: '/getInfo',
-    method: 'get'
-  })
-}
-
 // 退出方法
 export function logout() {
   return request({
