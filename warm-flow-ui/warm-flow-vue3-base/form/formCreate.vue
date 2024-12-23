caa41fa517a7c2268936d75dcda045fb789dcd69
Vittery <w205782390@163.com>
2024-12-23 17:30:55 +0800

    [updaet] 表单反显

--- warm-flow-ui/warm-flow-vue3-base/form/formCreate.vue
+++ warm-flow-ui/warm-flow-vue3-base/form/formCreate.vue
@@ -2,18 +2,17 @@
   <el-form ref="form" :model="form" :rules="rules" :disabled="disabled">
     <form-create
       class="formCreate"
-      ref="FormCreate"
-      :value.sync="formData"
-      v-model="fApi"
+      v-model="formData"
+      v-model:api="fApi"
       :rule="rule"
       :option="option"
       :disabled="disabled"
     ></form-create>
     <el-divider v-if="showApprovalFields"></el-divider>
     <el-form-item label="审批意见" prop="message" v-if="showApprovalFields">
-      <el-input v-model="form.message" type="textarea" placeholder="请输入审批意见" :autosize="{ minRows: 4, maxRows: 4 }" />
+      <el-input v-model="message" type="textarea" placeholder="请输入审批意见" :autosize="{ minRows: 4, maxRows: 4 }" />
     </el-form-item>
-    <div class="dialog-footer" v-if="showApprovalFields">
+    <div style="text-align: right;" v-if="showApprovalFields">
       <el-button type="primary" @click="handleBtn('PASS')">审批通过</el-button>
       <el-button @click="handleBtn('REJECT')">退回</el-button>
     </div>
@@ -27,22 +26,24 @@ const { proxy } = getCurrentInstance();
 const disabled = ref(false);
 const fApi = ref(null);
 const showApprovalFields = ref(true); // 控制审批意见和按钮是否显示
+const taskId = ref("");
+const message = ref("");
 const data = reactive({
   // 表单设计内容
   formData: {},
   rule: [],
   option: {},
-  form: {
-    message: ""
-  },
+  form: {},
   rules: {}
 });
 const { formData, rule, option, form, rules } = toRefs(data);
+
 window.addEventListener("message", handleMessage);
 window.parent.postMessage({ method: "formInit" }, "*");
 onBeforeUnmount(() => {
   window.removeEventListener('message', handleMessage);
 });
+
 function handleMessage(event) {
   switch (event.data.method) {
     case "formInit":
@@ -56,16 +57,15 @@ function handleMessage(event) {
 // 表单重置
 function reset() {
   formData.value = {};
-  form.value.message = "";
+  message.value = "";
 };
 /** 审核通过按钮 */
 function handleBtn(skipType) {
-  fApi.value.submit((formData, fApi) => {
+  fApi.value.submit(() => {
     proxy.$refs["form"].validate(valid => {
       if (valid) {
-        executeHandle(formData.value, props.taskId, skipType, form.value.message).then(response => {
-          proxy.$modal.msgSuccess("办理成功");
-          window.parent.postMessage({ method: "cancel" }, "*");
+        executeHandle(formData.value, taskId.value, skipType, message.value).then(response => {
+          window.parent.postMessage({ method: "submitSuccess" }, "*");
         });
       }
     });
@@ -75,17 +75,18 @@ function handleBtn(skipType) {
 async function formInit(data) {
   let response = null;
   let formContent = null;
+  taskId.value = data.taskId;
   // type 来源：0待办-办理 1已办-流程历史记录 2已发布的表单设计
   if (data.type === "0") {
     reset();
     response = await executeLoad(data.taskId);
     if (!response.data) proxy.$modal.alertWarning("待办任务不存在");
-    formContent = response.data;
+    formContent = JSON.parse(response.data.form?.formContent);
   } else if (data.type === "1") {
     showApprovalFields.value = false;
     response = await hisLoad(data.taskId);
     if (!response.data) proxy.$modal.alertWarning("历史记录不存在");
-    formContent = response.data;
+    formContent = JSON.parse(response.data.form?.formContent);
   } else {
     showApprovalFields.value = false;
     response = await getFormContent(data.formId);
@@ -96,9 +97,10 @@ async function formInit(data) {
   rule.value = formContent.rule;
   option.value = formContent.option;
   if (option.value) option.value.submitBtn = false;
-  formData.value = response.data; // 表单内容
+  formData.value = response.data.data; // 表单内容
+  message.value = response.data.data.message || "000000000000000";
   proxy.$nextTick(() => {
-    window.parent.postMessage({ method: "getOffsetHeight", offsetHeight: proxy.$refs.FormCreate.$el.offsetHeight }, "*");
+    window.parent.postMessage({ method: "getOffsetHeight", offsetHeight: proxy.$refs.form.$el.offsetHeight }, "*");
   });
 };
 </script>
